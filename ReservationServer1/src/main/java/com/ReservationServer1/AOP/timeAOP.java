24a13461bcf038a63f1fb5bcc1c51a25fd9cb7e5
JaeminMok <bamer@naver.com>
2023-04-24 15:59:28 +0900

    Testing

--- ReservationServer1/src/main/java/com/ReservationServer1/AOP/timeAOP.java
+++ ReservationServer1/src/main/java/com/ReservationServer1/AOP/timeAOP.java
@@ -3,10 +3,7 @@ package com.ReservationServer1.AOP;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import com.ReservationServer1.controller.MemberController;
 
 @Component
 @Aspect
