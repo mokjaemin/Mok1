d42fc3d586ea2b66ffc4b0c5adbd5cb35df228be
JaeminMok <bamer@naver.com>
2023-11-05 15:38:31 +0900

    Update

--- ReservationServer1/src/main/java/com/ReservationServer1/ReservationServer1Application.java
+++ ReservationServer1/src/main/java/com/ReservationServer1/ReservationServer1Application.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReservationServer1Application {
 
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationServer1Application.class, args);
 	}
